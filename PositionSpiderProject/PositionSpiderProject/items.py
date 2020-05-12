e4d12115ad66f08aa9ddde72f6685a7b44ebc377
范建海 <594042358@qq.com>
2020-05-12 20:15:06 +0800

    feat(master): 增加待爬取的字段

--- PositionSpiderProject/PositionSpiderProject/items.py
+++ PositionSpiderProject/PositionSpiderProject/items.py
@@ -9,6 +9,39 @@ import scrapy
 
 
 class PositionspiderprojectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    # 职位标题
+    title = scrapy.Field()
+    # 省份
+    province = scrapy.Field()
+    # 城市
+    city = scrapy.Field()
+    # 详细地址
+    address = scrapy.Field()
+    # 薪水
+    salary = scrapy.Field()
+    # 工作经验
+    experience = scrapy.Field()
+    # 学历
+    education = scrapy.Field()
+    # 工作类型
+    jobType = scrapy.Field()
+    # 发布时间
+    publishTime = scrapy.Field()
+    # 职位诱惑
+    welfare = scrapy.Field()
+    # 职位职责
+    jobResponsibilities = scrapy.Field()
+    # 职位要求
+    jobRequirements = scrapy.Field()
+    # 公司名称
+    companyName = scrapy.Field()
+    # 公司头像
+    companyIcon = scrapy.Field()
+    # 公司领域
+    companyField = scrapy.Field()
+    # 发展阶段
+    companyDevelopStage = scrapy.Field()
+    # 规模
+    companyScale = scrapy.Field()
+    # 主页
+    homePage = scrapy.Field()
