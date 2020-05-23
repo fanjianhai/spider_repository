e746987ac32c1e255d87ca47c1a81923091cfe92
范建海 <594042358@qq.com>
2020-05-23 11:46:15 +0800

    feat:修改输出excel文件路径

--- PositionSpiderProject/PositionSpiderProject/common_spider/position_to_excel.py
+++ PositionSpiderProject/PositionSpiderProject/common_spider/position_to_excel.py
@@ -1,7 +1,7 @@
 import copy
 
 from PositionSpiderProject.conf.common import DB_MEDCHAT_NAME, PROVINCE_CITY_2018, PROVINCE_CITY_2019, LAGOU, \
-    DB_POSITION_LAGOU
+    DB_POSITION_LAGOU, time_str
 from PositionSpiderProject.util.mongo_util import DBMongo
 from PositionSpiderProject.util.excel_util import save_to_excel
 
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     dbMongo = DBMongo(DB_POSITION_LAGOU, LAGOU)
     ret = dbMongo.find_no_condition()
     lst = [i for i in ret]
-    save_to_excel(lst, "拉钩网爬虫", "拉钩网爬虫", "G1")
+    save_to_excel(lst, "{}拉钩网it热门职位在热门城市招聘信息".format(time_str), "{}拉钩网it热门职位在热门城市招聘信息".format(time_str), "O1")
