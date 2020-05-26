0bcffa2ce369c5e52b527b4b3c17f829d638af84
范建海 <594042358@qq.com>
2020-05-26 11:51:25 +0800

    feat: 增加反扒代理

--- PositionSpiderProject/PositionSpiderProject/common_spider/demo.py
+++ PositionSpiderProject/PositionSpiderProject/common_spider/demo.py
@@ -1,4 +1,43 @@
-aaa = {}
-aaa['a']['b'] = 1
-print(aaa)
+import random
+import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from PositionSpiderProject.conf.user_agent import USER_AGENT_LIST
+
+url = "https://httpbin.org/ip"
+# 进入浏览器设置
+options = Options()
+# 设置中文
+options.add_argument('lang=zh_CN.UTF-8')
+options.add_argument('--headless')
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_argument('user-agent={}'.format(random.choice(USER_AGENT_LIST)))
+options.add_argument("--proxy-server=http://113.94.123.100:4287")
+# 更换头部
+driver_path = r"D:\chromedriver\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=driver_path, options=options)
+driver.get(url)
+# 获取当前路径
+print(driver.current_url)
+# 网页原内容
+print(driver.page_source)
+i = 0
+while True:
+    i += 1
+    options = Options()
+    options.add_argument('lang=zh_CN.UTF-8')
+    options.add_argument('--headless')
+    options.add_argument('user-agent={}'.format(random.choice(USER_AGENT_LIST)))
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    if i % 2 == 0:
+        options.add_argument("--proxy-server=http://1.180.165.250:4245")
+    else:
+        options.add_argument("--proxy-server=http://113.94.123.100:4287")
+
+    # driver = webdriver.Chrome(executable_path=driver_path, options=options)
+    driver.get(url)
+    # 网页原内容
+    print(driver.page_source)
+    time.sleep(2)
--- PositionSpiderProject/PositionSpiderProject/common_spider/position_lagou.py
+++ PositionSpiderProject/PositionSpiderProject/common_spider/position_lagou.py
@@ -1,8 +1,11 @@
 import json
+import logging
 import os
 import re
 import time
+from random import random
 from urllib.parse import quote
+import random
 
 from lxml import etree
 from selenium import webdriver
@@ -13,7 +16,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from PositionSpiderProject.conf.common import LAGOU, DB_POSITION_LAGOU, OUTPUT_JSON_DIR, JSON_NAME, FIELDS, \
-    INDEX_PAGE_0, CITY_CODES
+    INDEX_PAGE_0, CITY_CODES, CITY_LABELS
+from PositionSpiderProject.conf.user_agent import USER_AGENT_LIST
 from PositionSpiderProject.util.mongo_util import DBMongo
 
 
@@ -21,53 +25,68 @@ class LagouSpider(object):
     """
     Selenium + ChromeDriver 拉钩爬虫
     """
-    driver_path = r"D:\chromedriver\new\chromedriver.exe"
+    driver_path = r"D:\chromedriver\chromedriver.exe"
 
     def __init__(self):
-        options = Options()
-        options.add_argument('--headless')
-        self.driver = webdriver.Chrome(executable_path=LagouSpider.driver_path, options=options)
+        self.init_driver()
         self.mongo = DBMongo(DB_POSITION_LAGOU, LAGOU)
 
         if not os.path.exists(OUTPUT_JSON_DIR):
             os.makedirs(OUTPUT_JSON_DIR)
 
+    def init_driver(self):
+        options = Options()
+        options.add_argument('lang=zh_CN.UTF-8')
+        options.add_argument('--headless')
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        options.add_argument('user-agent={}'.format(random.choice(USER_AGENT_LIST)))
+        options.add_argument("--proxy-server=http://125.33.57.9:4281")
+        self.driver = webdriver.Chrome(executable_path=LagouSpider.driver_path, options=options)
+
     def run(self):
-        for i in range(len(FIELDS)):
-            for j in range(len(CITY_CODES)):
-                self.driver.get(INDEX_PAGE_0.format(quote(FIELDS[i]), CITY_CODES[j]))
-                while True:
-                    time.sleep(2)
-                    source = self.driver.page_source
+        # for i in range(len(FIELDS)):
+        #     for j in range(len(CITY_CODES)):
+            i = 0
+            j = 1
+            logging.error("我是i ==> {} ,我是j ==> {}".format(FIELDS[i],CITY_LABELS[j]))
+            self.driver.get(INDEX_PAGE_0.format(quote(FIELDS[i]), CITY_CODES[j]))
+            pre_page_url = None
+
+            while True:
+                time.sleep(random.randint(1,3))
+                source = self.driver.page_source
+                try:
+                    self.driver.find_element_by_xpath("//div[@class='qr_code_content']")
+                    self.init_driver()
+                    if pre_page_url is not None:
+                        self.driver.get(pre_page_url)
+                except NoSuchElementException as e:
+                    pass
+                try:
+                    WebDriverWait(self.driver, 7).until(
+                        # 这里只能追踪的元素，不能追踪到元素的具体属性
+                        EC.presence_of_element_located((By.XPATH, "//div[@class='pager_container']/span[last()]"))
+                    )
+                except Exception as e:
+                    pass
+
+                self.parse_list_page(source)
+                try:
+                    next_btn = self.driver.find_element_by_xpath("//div[@class='pager_container']/span[last()]")
+                except NoSuchElementException as e:
+                    break
+
+                if "pager_next_disabled" in next_btn.get_attribute("class"):
+                    break
+                else:
                     try:
-                        self.driver.find_element_by_xpath("//div[@class='qr_code_content']")
-                        continue
-                    except NoSuchElementException as e:
-                        pass
-                    try:
-                        WebDriverWait(self.driver, 7).until(
+                        pre_page_url = self.driver.current_url
+                        next_btn.click()
+                    except Exception as e:
+                        WebDriverWait(self.driver, 10).until(
                             # 这里只能追踪的元素，不能追踪到元素的具体属性
                             EC.presence_of_element_located((By.XPATH, "//div[@class='pager_container']/span[last()]"))
                         )
-                    except Exception as e:
-                        pass
-
-                    self.parse_list_page(source)
-                    try:
-                        next_btn = self.driver.find_element_by_xpath("//div[@class='pager_container']/span[last()]")
-                    except NoSuchElementException as e:
-                        break
-
-                    if "pager_next_disabled" in next_btn.get_attribute("class"):
-                        break
-                    else:
-                        try:
-                            next_btn.click()
-                        except Exception as e:
-                            WebDriverWait(self.driver, 60 * 60).until(
-                                # 这里只能追踪的元素，不能追踪到元素的具体属性
-                                EC.presence_of_element_located((By.XPATH, "//div[@class='pager_container']/span[last()]"))
-                            )
 
     def parse_list_page(self, source):
         """解析列表"""
@@ -80,7 +99,7 @@ class LagouSpider(object):
         current_page = 0
 
         for i in range(len(urls)):
-            time.sleep(2)
+            time.sleep(random.randint(1,3))
 
             self.driver.execute_script("window.open('{}')".format(urls[current_page]))
             self.driver.switch_to.window(self.driver.window_handles[1])
